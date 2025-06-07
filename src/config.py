d05a635f45d21d498704c6efd469222d383ee8ca
manhtd98 <manhtd6932@seta-international.vn>
2025-06-07 12:22:07 +0700

    update model

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import os
 PROMPT_TEMPLATE= """
 Answer the question based only on the following context:
 
@@ -12,8 +13,10 @@ Answer the question based on the above context in a brief: {question}
 class CONFIG:
     # "mxbai-embed-large"
     EMBEDDING_MODEL = "nomic-embed-text"
-    DATA_PDF_PATH = "data/"
-    CHROMA_PATH = "chroma"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    DATA_PDF_PATH = script_dir + "/data/"
+    CHROMA_PATH = script_dir + "/chroma"
     OLLAMA_RESPONSE_MODEL = "qwen3:4b"
     TOP_K = 5
     
